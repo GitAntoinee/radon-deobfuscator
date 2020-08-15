0c5fa1bfd3d78c0eef3680cdea83e9b728d86337
GitAntoinee <apottime@gmail.com>
2020-08-15 19:41:39 +0200

    Continue the deobfuscate function
    As there is no visitors it only copy the streams

--- src/main/kotlin/RadonDeobfuscator.kt
+++ src/main/kotlin/RadonDeobfuscator.kt
@@ -1,9 +1,16 @@
 package com.github.gitantoinee.deobfuscator.radon
 
+import org.objectweb.asm.ClassReader
+import org.objectweb.asm.ClassWriter
 import java.io.InputStream
 import java.io.OutputStream
 
 public class RadonDeobfuscator {
     public fun deobfuscate(inputStream: InputStream, outputStream: OutputStream) {
+        val reader = ClassReader(inputStream.readBytes())
+        val writer = ClassWriter(reader, 0).also {
+            reader.accept(it, 0)
+        }
+        outputStream.write(writer.toByteArray())
     }
 }
