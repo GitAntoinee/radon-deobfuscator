19b4f134089556e22c0ca917ace52198b4cada1c
GitAntoinee <apottime@gmail.com>
2020-08-15 20:52:23 +0200

    Add config value to invoke dynamic deobfuscator

--- src/main/kotlin/visitors/RadonInvokeDynamicMethodVisitor.kt
+++ src/main/kotlin/visitors/RadonInvokeDynamicMethodVisitor.kt
@@ -5,6 +5,10 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class RadonInvokeDynamicMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner) {
+    private companion object {
+        const val FAST: Boolean = true
+    }
+
     override fun visitInvokeDynamicInsn(
         name: String,
         descriptor: String,
