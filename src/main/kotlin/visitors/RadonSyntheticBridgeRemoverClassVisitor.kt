0e41b6cfdf105678ef2e23553891a89d43b1f71b
GitAntoinee <apottime@gmail.com>
2020-08-15 20:38:21 +0200

    Remove synthetic and bridge for methods

--- src/main/kotlin/visitors/RadonSyntheticBridgeRemoverClassVisitor.kt
+++ src/main/kotlin/visitors/RadonSyntheticBridgeRemoverClassVisitor.kt
@@ -1,7 +1,23 @@
 package com.github.gitantoinee.deobfuscator.radon.visitors
 
 import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class RadonSyntheticBridgeRemoverClassVisitor(inner: ClassVisitor? = null) : ClassVisitor(Opcodes.ASM9, inner) {
+    private companion object {
+        const val ADDED_ACCESS: Int = (Opcodes.ACC_SYNTHETIC or Opcodes.ACC_BRIDGE).inv()
+    }
+
+    override fun visitMethod(
+        access: Int,
+        name: String,
+        descriptor: String,
+        signature: String?,
+        exceptions: Array<out String>?,
+    ): MethodVisitor {
+        val newAccess = access and ADDED_ACCESS
+
+        return super.visitMethod(newAccess, name, descriptor, signature, exceptions)
+    }
 }
