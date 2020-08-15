7938848921122bde8d89b64ff1d0106dd59bce91
GitAntoinee <apottime@gmail.com>
2020-08-15 20:14:10 +0200

    Add radon illegal annotation deobfuscator to deobfuscation pipeline

--- src/main/kotlin/RadonDeobfuscator.kt
+++ src/main/kotlin/RadonDeobfuscator.kt
@@ -1,5 +1,6 @@
 package com.github.gitantoinee.deobfuscator.radon
 
+import com.github.gitantoinee.deobfuscator.radon.visitors.RadonIllegalAnnotationClassVisitor
 import com.github.gitantoinee.deobfuscator.radon.visitors.RadonInvokeDynamicClassVisitor
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
@@ -10,7 +11,7 @@ public class RadonDeobfuscator {
     public fun deobfuscate(inputStream: InputStream, outputStream: OutputStream) {
         val reader = ClassReader(inputStream.readBytes())
         val writer = ClassWriter(reader, 0).also {
-            reader.accept(RadonInvokeDynamicClassVisitor(it), 0)
+            reader.accept(RadonIllegalAnnotationClassVisitor(RadonInvokeDynamicClassVisitor(it)), 0)
         }
         outputStream.write(writer.toByteArray())
     }
