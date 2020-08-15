1677389e9ae0c245213c3ffbc54fe5d4a3792a52
GitAntoinee <apottime@gmail.com>
2020-08-15 19:40:17 +0200

    Add documentation to the alternative deobfuscate function

--- src/main/kotlin/RadonDeobfuscator.common.kt
+++ src/main/kotlin/RadonDeobfuscator.common.kt
@@ -3,6 +3,9 @@ package com.github.gitantoinee.deobfuscator.radon
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 
+/**
+ * Deobfuscate the input class [input] and return the deobfuscated class
+ */
 @Suppress("NOTHING_TO_INLINE")
 public inline fun RadonDeobfuscator.deobfuscate(input: ByteArray): ByteArray {
     return ByteArrayOutputStream().also {
