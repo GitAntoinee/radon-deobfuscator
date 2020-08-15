03d19df5c4ecf32a53a8448f11cb05f37c4a749e
GitAntoinee <apottime@gmail.com>
2020-08-15 20:18:22 +0200

    Add radon synthetic and bridge deobfuscator to deobfuscation pipeline

--- src/main/kotlin/RadonDeobfuscator.kt
+++ src/main/kotlin/RadonDeobfuscator.kt
@@ -2,6 +2,7 @@ package com.github.gitantoinee.deobfuscator.radon
 
 import com.github.gitantoinee.deobfuscator.radon.visitors.RadonIllegalAnnotationClassVisitor
 import com.github.gitantoinee.deobfuscator.radon.visitors.RadonInvokeDynamicClassVisitor
+import com.github.gitantoinee.deobfuscator.radon.visitors.RadonSyntheticBridgeRemoverClassVisitor
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import java.io.InputStream
@@ -11,7 +12,7 @@ public class RadonDeobfuscator {
     public fun deobfuscate(inputStream: InputStream, outputStream: OutputStream) {
         val reader = ClassReader(inputStream.readBytes())
         val writer = ClassWriter(reader, 0).also {
-            reader.accept(RadonIllegalAnnotationClassVisitor(RadonInvokeDynamicClassVisitor(it)), 0)
+            reader.accept(RadonSyntheticBridgeRemoverClassVisitor(RadonIllegalAnnotationClassVisitor(RadonInvokeDynamicClassVisitor(it))), 0)
         }
         outputStream.write(writer.toByteArray())
     }
