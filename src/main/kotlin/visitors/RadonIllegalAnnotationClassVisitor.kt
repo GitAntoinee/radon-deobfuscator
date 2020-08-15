898f665c9ddcc259ae725046354ee9e24e79234c
GitAntoinee <apottime@gmail.com>
2020-08-15 20:13:42 +0200

    Visit annotation only if it is legal

--- src/main/kotlin/visitors/RadonIllegalAnnotationClassVisitor.kt
+++ src/main/kotlin/visitors/RadonIllegalAnnotationClassVisitor.kt
@@ -1,7 +1,11 @@
 package com.github.gitantoinee.deobfuscator.radon.visitors
 
+import org.objectweb.asm.AnnotationVisitor
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.Opcodes
 
 public class RadonIllegalAnnotationClassVisitor(inner: ClassVisitor? = null) : ClassVisitor(Opcodes.ASM9, inner) {
+    override fun visitAnnotation(descriptor: String, visible: Boolean): AnnotationVisitor? {
+        return if ("@" != descriptor && "" != descriptor) super.visitAnnotation(descriptor, visible) else null
+    }
 }
