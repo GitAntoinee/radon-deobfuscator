290f1960c0bedc1c7a598ad3f585f58cfc2a40bd
GitAntoinee <apottime@gmail.com>
2020-08-15 19:45:16 +0200

    Add invoke dynamic deobfuscator to deobfuscation pipeline

--- src/main/kotlin/RadonDeobfuscator.kt
+++ src/main/kotlin/RadonDeobfuscator.kt
@@ -1,5 +1,6 @@
 package com.github.gitantoinee.deobfuscator.radon
 
+import com.github.gitantoinee.deobfuscator.radon.visitors.RadonInvokeDynamicClassVisitor
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
 import java.io.InputStream
@@ -9,7 +10,7 @@ public class RadonDeobfuscator {
     public fun deobfuscate(inputStream: InputStream, outputStream: OutputStream) {
         val reader = ClassReader(inputStream.readBytes())
         val writer = ClassWriter(reader, 0).also {
-            reader.accept(it, 0)
+            reader.accept(RadonInvokeDynamicClassVisitor(it), 0)
         }
         outputStream.write(writer.toByteArray())
     }
