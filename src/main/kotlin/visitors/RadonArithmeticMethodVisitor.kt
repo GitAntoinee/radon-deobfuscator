4eed714fc9671cfd1bdcfb0d0afa895560343ce0
GitAntoinee <apottime@gmail.com>
2020-08-16 00:02:31 +0200

    Handle all type of value for an ldc opcode

--- src/main/kotlin/visitors/RadonArithmeticMethodVisitor.kt
+++ src/main/kotlin/visitors/RadonArithmeticMethodVisitor.kt
@@ -1,7 +1,6 @@
 package com.github.gitantoinee.deobfuscator.radon.visitors
 
-import org.objectweb.asm.MethodVisitor
-import org.objectweb.asm.Opcodes
+import org.objectweb.asm.*
 
 public class RadonArithmeticMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner) {
     private var sum: Int = 0
@@ -38,8 +37,8 @@ public class RadonArithmeticMethodVisitor(inner: MethodVisitor? = null) : Method
     override fun visitLdcInsn(value: Any) {
         when (value) {
             is Number -> operand = value
-            is String -> sum = 0
-            else -> error("Load constant operand is not a number or a string")
+            is String, is Type, is Handle, is ConstantDynamic -> sum = 0
+            else -> error("Load constant operand is invalid")
         }
     }
 }
