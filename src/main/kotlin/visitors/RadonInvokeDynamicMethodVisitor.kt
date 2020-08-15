c7b8d2d1c0d297b345e9090616b70ddd4db4441e
GitAntoinee <apottime@gmail.com>
2020-08-15 19:55:43 +0200

    Log invoke dynamic instructions

--- src/main/kotlin/visitors/RadonInvokeDynamicMethodVisitor.kt
+++ src/main/kotlin/visitors/RadonInvokeDynamicMethodVisitor.kt
@@ -1,7 +1,19 @@
 package com.github.gitantoinee.deobfuscator.radon.visitors
 
+import org.objectweb.asm.Handle
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class RadonInvokeDynamicMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner) {
+    override fun visitInvokeDynamicInsn(
+        name: String,
+        descriptor: String,
+        bootstrapMethodHandle: Handle,
+        vararg bootstrapMethodArguments: Any,
+    ) {
+        println("INDY ($bootstrapMethodHandle) : `$name` `$descriptor` " +
+                bootstrapMethodArguments.joinToString(" ", "`", "`"))
+
+        super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, *bootstrapMethodArguments)
+    }
 }
