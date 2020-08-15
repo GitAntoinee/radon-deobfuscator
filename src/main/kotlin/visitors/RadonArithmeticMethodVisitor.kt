e54097326522fb72d6c2b7dcf4d86643bf528362
GitAntoinee <apottime@gmail.com>
2020-08-15 23:25:01 +0200

    Begin radon arithmetic deobfuscator

---
+++ src/main/kotlin/visitors/RadonArithmeticMethodVisitor.kt
@@ -0,0 +1,6 @@
+package com.github.gitantoinee.deobfuscator.radon.visitors
+
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+
+public class RadonArithmeticMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner)
