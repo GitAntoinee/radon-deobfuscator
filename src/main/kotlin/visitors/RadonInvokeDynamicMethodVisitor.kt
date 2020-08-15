520277f53733d656faec436c806e50872e071579
GitAntoinee <apottime@gmail.com>
2020-08-15 22:33:06 +0200

    Deobfuscate invoke dynamics

--- src/main/kotlin/visitors/RadonInvokeDynamicMethodVisitor.kt
+++ src/main/kotlin/visitors/RadonInvokeDynamicMethodVisitor.kt
@@ -10,6 +10,16 @@ public class RadonInvokeDynamicMethodVisitor(inner: MethodVisitor? = null) : Met
          * Depend on the obfuscation, if it use fast invoke dynamic or normal invoke dynamic
          */
         const val FAST: Boolean = true
+
+        /**
+         * Number used to define a static method by radon
+         */
+        const val STATIC_METHOD_TYPE: Int = 0
+
+        /**
+         * Number used to define a virtual method by radon
+         */
+        const val VIRTUAL_METHOD_TYPE: Int = 1
     }
 
     override fun visitInvokeDynamicInsn(
@@ -22,7 +32,25 @@ public class RadonInvokeDynamicMethodVisitor(inner: MethodVisitor? = null) : Met
                 && bootstrapMethodArguments.drop(1).all { it is String }
 
         if (isObfuscated) {
-            // Deobfuscation will be here
+            // TODO : Find xor encryption
+
+            val originalOwner = bootstrapMethodArguments[1].toString().map {
+                it.toInt() xor 2893
+            }.joinToString("") { it.toChar().toString() }
+            val originalName = bootstrapMethodArguments[2].toString().map {
+                it.toInt() xor 2993
+            }.joinToString("") { it.toChar().toString() }
+            val originalDescriptor = bootstrapMethodArguments[3].toString().map {
+                it.toInt() xor 8372
+            }.joinToString("") { it.toChar().toString() }
+
+            val originalOpcode = when ((bootstrapMethodArguments[0] as Int) shl 256 and 255) {
+                STATIC_METHOD_TYPE -> Opcodes.INVOKESTATIC
+                VIRTUAL_METHOD_TYPE -> Opcodes.INVOKEVIRTUAL
+                else -> error("Unknown obfuscated type")
+            }
+
+            super.visitMethodInsn(originalOpcode, originalOwner, originalName, originalDescriptor, false)
         } else {
             super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, *bootstrapMethodArguments)
         }
