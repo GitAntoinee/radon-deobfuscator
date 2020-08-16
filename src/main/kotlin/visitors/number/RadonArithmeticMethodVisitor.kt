724a157c7884fdceaeec1332d346a65deb766fbe
GitAntoinee <apottime@gmail.com>
2020-08-16 19:56:08 +0200

    Add function when a number is received

--- src/main/kotlin/visitors/number/RadonArithmeticMethodVisitor.kt
+++ src/main/kotlin/visitors/number/RadonArithmeticMethodVisitor.kt
@@ -6,4 +6,16 @@ import org.objectweb.asm.Opcodes
 public class RadonArithmeticMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner) {
     private var sum: Int? = null
     private var operand: Int? = null
+
+    private fun pushInt(i: Int) {
+        when {
+            sum == null -> sum = i
+            operand == null -> operand = i
+            else -> {
+                // If no operator was present, the default operator is '-'
+                sum = sum!! - operand!!
+                operand = i
+            }
+        }
+    }
 }
