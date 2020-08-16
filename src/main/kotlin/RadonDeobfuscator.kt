d46b0fb28735df3f1cadd2d15de13c917fd01c0a
GitAntoinee <apottime@gmail.com>
2020-08-16 14:26:00 +0200

    Remove the Arithmetic deobfuscator

--- src/main/kotlin/RadonDeobfuscator.kt
+++ src/main/kotlin/RadonDeobfuscator.kt
@@ -4,7 +4,6 @@ import com.github.gitantoinee.deobfuscator.radon.visitors.RadonBadAnnotationClas
 import com.github.gitantoinee.deobfuscator.radon.visitors.RadonHideCodeClassVisitor
 import com.github.gitantoinee.deobfuscator.radon.visitors.flow.RadonBogusJumpInserterClassVisitor
 import com.github.gitantoinee.deobfuscator.radon.visitors.flow.RadonGotoReplacerClassVisitor
-import com.github.gitantoinee.deobfuscator.radon.visitors.number.RadonArithmeticClassVisitor
 import com.github.gitantoinee.deobfuscator.radon.visitors.references.RadonInvokeDynamicClassVisitor
 import org.objectweb.asm.ClassReader
 import org.objectweb.asm.ClassWriter
@@ -19,9 +18,8 @@ public class RadonDeobfuscator {
                 RadonHideCodeClassVisitor(
                     RadonBogusJumpInserterClassVisitor(
                         RadonGotoReplacerClassVisitor(
-                            RadonArithmeticClassVisitor(
                                 RadonBadAnnotationClassVisitor(
-                                    RadonInvokeDynamicClassVisitor(it)))))),
+                                    RadonInvokeDynamicClassVisitor(it))))),
                 0)
         }
         outputStream.write(writer.toByteArray())
