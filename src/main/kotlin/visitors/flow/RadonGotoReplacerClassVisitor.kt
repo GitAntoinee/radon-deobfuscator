f31ec39087881de8d2d62b1be7f0f7d5409b52f6
GitAntoinee <apottime@gmail.com>
2020-08-16 10:31:12 +0200

    Create a list for the possible predicate fields

--- src/main/kotlin/visitors/flow/RadonGotoReplacerClassVisitor.kt
+++ src/main/kotlin/visitors/flow/RadonGotoReplacerClassVisitor.kt
@@ -5,6 +5,8 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class RadonGotoReplacerClassVisitor(inner: ClassVisitor? = null) : ClassVisitor(Opcodes.ASM9, inner) {
+    private val possiblePredicateFields: MutableList<String> = mutableListOf()
+
     override fun visitMethod(
         access: Int,
         name: String?,
@@ -14,6 +16,6 @@ public class RadonGotoReplacerClassVisitor(inner: ClassVisitor? = null) : ClassV
     ): MethodVisitor {
         val inner = super.visitMethod(access, name, descriptor, signature, exceptions)
 
-        return RadonGotoReplacerMethodVisitor(inner)
+        return RadonGotoReplacerMethodVisitor(possiblePredicateFields, inner)
     }
 }
