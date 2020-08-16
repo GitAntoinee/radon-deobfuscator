950c9f6a5fb4b8523902ae5f8b874b5f08cb97b2
GitAntoinee <apottime@gmail.com>
2020-08-16 17:41:28 +0200

    Add the predicateField as parameter to the primary constructor

--- src/main/kotlin/visitors/flow/RadonGotoReplacerMethodVisitor.kt
+++ src/main/kotlin/visitors/flow/RadonGotoReplacerMethodVisitor.kt
@@ -4,6 +4,10 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class RadonGotoReplacerMethodVisitor(
+    /**
+     * The owner, the name and the descriptor of the predicate field
+     */
+    private val predicateField: Triple<String, String, String>,
     inner: MethodVisitor? = null,
 ) : MethodVisitor(Opcodes.ASM9, inner) {
 }
