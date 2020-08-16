1b1b0bb4bd28d8b2db60f7698ce11ac00ed79f51
GitAntoinee <apottime@gmail.com>
2020-08-16 19:51:44 +0200

    Add sum and operand fields

--- src/main/kotlin/visitors/number/RadonArithmeticMethodVisitor.kt
+++ src/main/kotlin/visitors/number/RadonArithmeticMethodVisitor.kt
@@ -4,4 +4,6 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class RadonArithmeticMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner) {
+    private var sum: Int? = null
+    private var operand: Int? = null
 }
