3a749099032f64430cc7b54494b008805ef98927
GitAntoinee <apottime@gmail.com>
2020-08-16 10:09:33 +0200

    Begin finding of the predicate variable for the GotoReplacer deobfuscator

--- src/main/kotlin/visitors/flow/RadonGotoReplacerMethodVisitor.kt
+++ src/main/kotlin/visitors/flow/RadonGotoReplacerMethodVisitor.kt
@@ -3,4 +3,15 @@ package com.github.gitantoinee.deobfuscator.radon.visitors.flow
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
-public class RadonGotoReplacerMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner)
+public class RadonGotoReplacerMethodVisitor(
+    /**
+     * The possible predicate fields
+     */
+    private val possiblePredicateFields: List<String>,
+    inner: MethodVisitor? = null,
+) : MethodVisitor(Opcodes.ASM9, inner) {
+    /**
+     * The name and the index of variable used as operand for IFEQ opcodes
+     */
+    private var predicateVar: Pair<String, Int>? = null
+}
