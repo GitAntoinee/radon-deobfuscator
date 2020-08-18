efddbb5ced883d1b51e3b536f65be505f69c67cb
GitAntoinee <apottime@gmail.com>
2020-08-18 16:56:33 +0200

    Store the descriptor of the class to a field

--- src/main/kotlin/visitors/RadonLocalVariableRenamerClassVisitor.kt
+++ src/main/kotlin/visitors/RadonLocalVariableRenamerClassVisitor.kt
@@ -3,8 +3,24 @@ package com.github.gitantoinee.deobfuscator.radon.visitors
 import org.objectweb.asm.ClassVisitor
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
 
 public class RadonLocalVariableRenamerClassVisitor(inner: ClassVisitor? = null) : ClassVisitor(Opcodes.ASM9, inner) {
+    private lateinit var classDescriptor: String
+
+    override fun visit(
+        version: Int,
+        access: Int,
+        name: String,
+        signature: String?,
+        superName: String?,
+        interfaces: Array<out String>?,
+    ) {
+        classDescriptor = Type.getObjectType(name).descriptor
+
+        super.visit(version, access, name, signature, superName, interfaces)
+    }
+
     override fun visitMethod(
         access: Int,
         name: String?,
