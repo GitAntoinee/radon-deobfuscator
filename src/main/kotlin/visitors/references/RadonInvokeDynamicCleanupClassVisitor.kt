a6fbe3c2cf49ba6d360d3851314319ad81822855
GitAntoinee <apottime@gmail.com>
2020-08-19 12:26:48 +0200

    Compare the bootstrap method handle owners and the current method owner

--- src/main/kotlin/visitors/references/RadonInvokeDynamicCleanupClassVisitor.kt
+++ src/main/kotlin/visitors/references/RadonInvokeDynamicCleanupClassVisitor.kt
@@ -12,6 +12,21 @@ public class RadonInvokeDynamicCleanupClassVisitor(
     private val bootstrapMethods: Set<Handle>,
     inner: ClassVisitor? = null,
 ) : ClassVisitor(Opcodes.ASM9, inner) {
+    private lateinit var owner: String
+
+    override fun visit(
+        version: Int,
+        access: Int,
+        name: String,
+        signature: String?,
+        superName: String?,
+        interfaces: Array<out String>?,
+    ) {
+        owner = name
+
+        super.visit(version, access, name, signature, superName, interfaces)
+    }
+
     override fun visitMethod(
         access: Int,
         name: String?,
@@ -19,7 +34,7 @@ public class RadonInvokeDynamicCleanupClassVisitor(
         signature: String?,
         exceptions: Array<out String>?,
     ): MethodVisitor? {
-        return if (bootstrapMethods.all { it.name != name && it.desc != descriptor }) {
+        return if (bootstrapMethods.all { it.owner != owner && it.name != name && it.desc != descriptor }) {
             super.visitMethod(access, name, descriptor, signature, exceptions)
         } else {
             null
