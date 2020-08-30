be1d711b22e0a354dbac119fc36944b9c4aa153e
GitAntoinee <apottime@gmail.com>
2020-08-30 18:12:40 +0200

    Store the possible fields who have the encrypted strings

--- src/main/kotlin/visitors/string/LightStringEncryptionClassVisitor.kt
+++ src/main/kotlin/visitors/string/LightStringEncryptionClassVisitor.kt
@@ -1,6 +1,7 @@
 package com.github.gitantoinee.deobfuscator.radon.visitors.string
 
 import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.FieldVisitor
 import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
@@ -8,6 +9,16 @@ public class LightStringEncryptionClassVisitor(inner: ClassVisitor? = null) : Cl
     private companion object {
         private const val ENCRYPTED_STRINGS_FIELD_DESCRIPTOR: String = "[Ljava/lang/String;"
         private const val ENCRYPTED_STRINGS_FIELD_ACCESS: Int = Opcodes.ACC_PRIVATE and Opcodes.ACC_STATIC
+    }
+
+    private val possibleEncryptedStringsFields: MutableList<String> = mutableListOf()
+
+    override fun visitField(access: Int, name: String, descriptor: String, signature: String?, value: Any?): FieldVisitor {
+        if (ENCRYPTED_STRINGS_FIELD_ACCESS == access && ENCRYPTED_STRINGS_FIELD_DESCRIPTOR == descriptor) {
+            possibleEncryptedStringsFields.add(name)
+        }
+
+        return super.visitField(access, name, descriptor, signature, value)
     }
 
     override fun visitMethod(access: Int, name: String?, descriptor: String?, signature: String?, exceptions: Array<out String>?): MethodVisitor {
