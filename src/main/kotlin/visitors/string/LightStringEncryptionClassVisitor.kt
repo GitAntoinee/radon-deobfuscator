20097a659ed79247c9d224547707be3558f4b076
GitAntoinee <apottime@gmail.com>
2020-08-30 18:07:31 +0200

    Add a constant value who store the descriptor of the encrypted strings

--- src/main/kotlin/visitors/string/LightStringEncryptionClassVisitor.kt
+++ src/main/kotlin/visitors/string/LightStringEncryptionClassVisitor.kt
@@ -5,6 +5,10 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class LightStringEncryptionClassVisitor(inner: ClassVisitor? = null) : ClassVisitor(Opcodes.ASM9, inner) {
+    private companion object {
+        private const val ENCRYPTED_STRINGS_FIELD_DESCRIPTOR: String = "[Ljava/lang/String;"
+    }
+
     override fun visitMethod(access: Int, name: String?, descriptor: String?, signature: String?, exceptions: Array<out String>?): MethodVisitor {
         val inner = super.visitMethod(access, name, descriptor, signature, exceptions)
 
