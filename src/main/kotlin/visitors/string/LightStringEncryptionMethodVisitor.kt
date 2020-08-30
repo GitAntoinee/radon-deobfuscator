56c19d3a8c9e3a184b767359410e0f75fad2eb45
GitAntoinee <apottime@gmail.com>
2020-08-30 17:56:05 +0200

    Add a constant value who store the descriptor for all decryption methods

--- src/main/kotlin/visitors/string/LightStringEncryptionMethodVisitor.kt
+++ src/main/kotlin/visitors/string/LightStringEncryptionMethodVisitor.kt
@@ -4,4 +4,7 @@ import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes
 
 public class LightStringEncryptionMethodVisitor(inner: MethodVisitor? = null) : MethodVisitor(Opcodes.ASM9, inner) {
+    private companion object {
+        const val DECRYPTION_METHOD_DESCRIPTOR: String = "(Ljava/lang/Object;I)Ljava/lang/String;"
+    }
 }
